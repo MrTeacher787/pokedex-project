2b469d0ce5431ce3ae8128ecc031d572f21c837b
Ivan G. Irizarry <cristophergiovanni@gmail.com>
2024-12-15 22:42:32 -0600

    Update scripts.js
    updated array of strings format and gave Pikachu an additional category

--- js/scripts.js
+++ js/scripts.js
@@ -2,26 +2,26 @@ let pokemonList = [
     {
       name: "psyduck",
       height: 0.8,
-      type: ["water", "psychic"]
+      type: ['water', 'psychic']
     }, 
     {
       name: "charmander",
       height: 0.6,
-      type: ["fire", "dragon"]
+      type: ['fire', 'dragon']
     }, 
     {
       name: "squirtle",
       height: 0.5,
-      type: ["water", "monster"]
+      type: ['water', 'monster']
     },
     {
       name: "bulbasaur",
       height: 0.7,
-      type: ["water", "monster"]
+      type: ['water', 'monster']
     },
     {
       name: "pikachu",
       height: 0.4,
-      type: "lightning",
+      type: ['lightning', 'mouse']
     }
 ];
