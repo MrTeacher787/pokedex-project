279682e46b0bebd3e11ebdfb2803da5ba4436506
Ivan G. Irizarry <cristophergiovanni@gmail.com>
2024-12-15 22:11:02 -0600

    Update scripts.js
    added data on 5 pokemon to the pokedex-project

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,27 @@
-alert('Hello World!');
-
-let favoriteFood = 'Any kind of pasta!';
-document.write(favoriteFood);
-
+let pokemonList = [
+    {
+      name: "psyduck",
+      height: 0.8,
+      type: ["water", "psychic"]
+    }, 
+    {
+      name: "charmander",
+      height: 0.6,
+      type: ["fire", "dragon"]
+    }, 
+    {
+      name: "squirtle",
+      height: 0.5,
+      type: ["water", "monster"]
+    },
+    {
+      name: "bulbasaur",
+      height: 0.7,
+      type: ["water", "monster"]
+    },
+    {
+      name: "pikachu",
+      height: 0.4,
+      type: "lightning",
+    }
+];
