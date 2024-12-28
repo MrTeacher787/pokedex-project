abb96b0d342f04daf1f8433079cb3c3597cb45b6
Ivan G. Irizarry <cristophergiovanni@gmail.com>
2024-12-28 14:07:11 -0600

    Updated Pokemon heights

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,27 @@
 let pokemonList = [
     {
       name: "psyduck",
-      height: 0.8,
+      height: 1.2,
       type: ['water', 'psychic']
     }, 
     {
       name: "charmander",
-      height: 0.6,
+      height: 2.1,
       type: ['fire', 'dragon']
     }, 
     {
       name: "squirtle",
-      height: 0.5,
+      height: 1.7,
       type: ['water', 'monster']
     },
     {
       name: "bulbasaur",
-      height: 0.7,
+      height: 0.9,
       type: ['water', 'monster']
     },
     {
       name: "pikachu",
-      height: 0.4,
+      height: 1.3,
       type: ['lightning', 'mouse']
     }
 ];
